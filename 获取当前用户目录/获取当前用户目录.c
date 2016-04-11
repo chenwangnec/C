09e701a4b54f92dc317493cdf044909e5d50452d
chenwang <chenwangnec@163.com>
2016-04-11 08:46:18 +0800

    备注暂时无用代码

--- 获取当前用户目录/获取当前用户目录.c
+++ 获取当前用户目录/获取当前用户目录.c
@@ -3,9 +3,9 @@
 
 int main(int argc, char* argv[])
 {
-	DWORD jisuanjiname = MAX_COMPUTERNAME_LENGTH + 1;
+	//DWORD jisuanjiname = MAX_COMPUTERNAME_LENGTH + 1;//(暂时无用)
 	DWORD username = 30;
-	char Jisuanjiname[MAX_COMPUTERNAME_LENGTH + 1];
+	//char Jisuanjiname[MAX_COMPUTERNAME_LENGTH + 1];//(暂时无用)
 	char susername[30];
 	GetUserNameA(susername, &username);
 	printf("用户名:%s\n", susername);
